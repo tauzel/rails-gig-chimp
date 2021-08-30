5c4fdd66b72d654b6be77b2ed0ccbe21c9a24a0c
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-30 19:31:22 +0200

    created students table

---
+++ db/migrate/20210830171043_create_students_table.rb
@@ -0,0 +1,13 @@
+class CreateStudentsTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :students_tables do |t|
+      t.string :name, null: false, unique: true
+      t.string :picture_url
+      t.boolean :opt_in, null: false, default: true
+      t.text :description
+      t.integer :batch
+
+      t.timestamps
+    end
+  end
+end
