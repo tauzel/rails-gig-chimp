8a8f2013beebbc285582aafd7f88cba7375a7c04
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-30 19:54:27 +0200

    name - null:false

--- db/migrate/20210830171043_create_students_table.rb
+++ db/migrate/20210830171043_create_students_table.rb
@@ -2,7 +2,7 @@ class CreateStudentsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :students_tables do |t|
       t.string :username, null: false, unique: true
-      t.string :name
+      t.string :name, null: false
       t.string :picture_url
       t.boolean :opt_in, null: false, default: true
       t.text :description
