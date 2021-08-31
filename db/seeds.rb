71e0caf5bedf494a16eb137d97c60e49d42bfb70
Henrique Albuquerque <henrique.alb@me.com>
2021-08-31 15:16:48 +0100

    Fixed user profile pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,19 @@
 
 require 'faker'
 
-50.times do
+puts 'Deleting database content'
+Student.destroy_all
+
+puts 'Generating new students'
+
+50.times do |i|
   name = Faker::Name.name
   username = Faker::Internet.username(specifier: name)
   description = Faker::Hipster.paragraph(sentence_count: 3)
-  picture = "https://kitt.lewagon.com/placeholder/users/random"
+  picture = "https://kitt.lewagon.com/placeholder/users/random#{i}"
   batch = rand(1..683)
 
   Student.create!(username: username, name: name, picture_url: picture, opt_in: 1, description: description, batch: batch)
 end
+
+puts 'All done!'
