b2884732f3a343c1a09ce7d9e02dba709a22dd1a
Artur Renzenbrink <artur.renzenbrink@gmail.com>
2021-08-31 13:06:51 +0200

    added the gigs migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_183416) do
+ActiveRecord::Schema.define(version: 2021_08_31_101615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gigs", force: :cascade do |t|
+    t.string "name"
+    t.date "start"
+    t.date "end"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_gigs_on_user_id"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
@@ -45,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_08_30_183416) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "gigs", "users"
 end
