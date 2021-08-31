979a0e9576c74c6ae515d4e0ed15365b6cd02f65
Thierry AUZEL <thierry.auzel@gmail.com>
2021-08-31 12:13:59 +0200

    made students username uniq at db level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_183416) do
+ActiveRecord::Schema.define(version: 2021_08_31_100527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_183416) do
     t.integer "batch"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_students_on_username", unique: true
   end
 
   create_table "users", force: :cascade do |t|
