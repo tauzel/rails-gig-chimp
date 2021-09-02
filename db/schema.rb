fbdb7fd3c5e1eb90193a3cdd7bcd09e205957fe9
Pedro Rato <rato.pedromoreira@gmail.com>
2021-09-02 15:32:55 +0100

    added Location column to Students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_132345) do
+ActiveRecord::Schema.define(version: 2021_09_02_143132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_132345) do
     t.integer "batch"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
     t.index ["username"], name: "index_students_on_username", unique: true
   end
 
@@ -68,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_132345) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.bigint "student_id"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["student_id"], name: "index_users_on_student_id"
