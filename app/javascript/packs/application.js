4425f6460a03e3bba59847d38766582b33f2a16e
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-02 13:15:03 +0200

    fixed js bug on card selection + put it in dedicated js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initCardSelect } from '../components/select_cards';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initCardSelect();
+
 });
