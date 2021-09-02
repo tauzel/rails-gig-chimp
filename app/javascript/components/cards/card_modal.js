52530d410b1e55d1aba6618027249a4b74a50641
Thierry AUZEL <thierry.auzel@gmail.com>
2021-09-02 23:10:00 +0200

    factored JS code into 2 separate files -- starting Student modal show function

---
+++ app/javascript/components/cards/card_modal.js
@@ -0,0 +1,12 @@
+import { linkClickSelector } from "@rails/ujs";
+
+const initModalShow = (cardInnerLinks) => {
+  // Retrieve student id
+  const studentId = cardInnerLinks.dataset["studentId"];
+
+  // Build student url with it
+
+  console.log(studentId)
+}
+
+export { initModalShow };
